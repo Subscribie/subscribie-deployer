21bf01c7e4d098c6b2c2ad17f1e6d9360fcdc29d
chrisjsimpson <chris@karmacomputing.co.uk>
2021-05-02 15:16:50 +0100

    auto refresh subscription statuses via uwsgi cron
    ref https://github.com/Subscribie/subscribie/issues/526

--- main.py
+++ main.py
@@ -421,8 +421,14 @@ def deploy():
 
     sed_inplace(
         vassalConfigFile,
-        r"cron2.*",
+        r"cron2.*announce-stripe-connect",
         fr"cron2 = minute=-1 curl -L {webaddress}\/admin\/announce-stripe-connect\n",  # noqa: E501
+    )
+
+    sed_inplace(
+        vassalConfigFile,
+        r"cron2.*refresh-subscription-statuses",
+        fr"cron2 = minute=-1 curl -L {webaddress}\/admin\/refresh-subscription-statuses\n",  # noqa: E501
     )
 
     sed_inplace(
